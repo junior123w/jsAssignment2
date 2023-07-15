7edf6c1a6658bf61107799316dc5f6e97c5694bd
junior123w <113151392+junior123w@users.noreply.github.com>
2023-07-14 23:26:34 -0400

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,65 @@
+const imageList = [
+  {
+    src: "./images/flowers-pink-large.jpg",
+    alt: "Pink Flowers",
+    thumbnailSrc: "./images/flowers-pink-small.jpg",
+    width: 1200,
+    height: 800
+  },
+  {
+    src: "./images/flowers-purple-large.jpg",
+    alt: "Purple Flowers",
+    thumbnailSrc: "./images/flowers-purple-small.jpg",
+    width: 1200,
+    height: 800
+  },
+  {
+    src: "./images/flowers-red-large.jpg",
+    alt: "Red Flowers",
+    thumbnailSrc: "./images/flowers-red-small.jpg",
+    width: 1200,
+    height: 800
+  },
+  {
+    src: "./images/flowers-white-large.jpg",
+    alt: "White Flowers",
+    thumbnailSrc: "./images/flowers-white-small.jpg",
+    width: 1200,
+    height: 800
+  },
+  {
+    src: "./images/flowers-yellow-large.jpg",
+    alt: "Yellow Flowers",
+    thumbnailSrc: "./images/flowers-yellow-small.jpg",
+    width: 1200,
+    height: 800
+  }
+];
+
+const thumbnailsContainer = document.querySelector(".thumbnails-container");
+const featuredImage = document.querySelector("figure img");
+const caption = document.querySelector("figure figcaption");
+
+function createThumbnail(image) {
+  const thumbnailImg = document.createElement("img");
+  thumbnailImg.src = image.thumbnailSrc;
+  thumbnailImg.alt = image.alt;
+  thumbnailImg.width = 240;
+  thumbnailImg.height = 160;
+  thumbnailImg.classList.add("thumbnail");
+  thumbnailImg.addEventListener("click", function () {
+    featuredImage.src = image.src;
+    featuredImage.alt = image.alt;
+    caption.textContent = image.alt;
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    thumbnails.forEach(function (thumbnail) {
+      thumbnail.classList.remove("active");
+    });
+    thumbnailImg.classList.add("active");
+  });
+  thumbnailsContainer.appendChild(thumbnailImg);
+}
+
+imageList.forEach(function (image) {
+  createThumbnail(image);
+});
